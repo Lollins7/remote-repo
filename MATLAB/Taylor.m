0d64673526a6e19898615cdd5dfb071ad2d9096f
Lollins <95170148+Lollins7@users.noreply.github.com>
2023-11-11 16:15:40 +0800

    Delete MATLAB directory

--- MATLAB/Taylor.m
+++
@@ -1,6 +0,0 @@
-t = [1/sqrt(2),1/sqrt(6),-1/sqrt(12),1/2;
-1/sqrt(2),-1/sqrt(6),1/sqrt(12),-1/2;
-0,2/sqrt(6),1/sqrt(12),-1/2;
-0,0,3/sqrt(12),1/2];
-x = linspace(1,10,10)
-plot(x,sin(x))
--- MATLAB/sishoukui/chapter1/problem1.m
+++
@@ -1,6 +0,0 @@
-clear,clc;
-f = [3;-1;-1];
-a = [1,-2,1;4,-1,-2];b = [11;-3];
-aeq = [-2,0,1];beq = [1];
-[x,z] = linprog(-f,a,b,aeq,beq,zeros(3,1))
-z = -z
